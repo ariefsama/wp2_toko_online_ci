37214ad6086cb6238949be7c3c2978b0821d0aae
SitiMelisaaa <19215174@bsi.ac.id>
2023-06-24 16:49:16 +0700

    detail produk

--- application/models/Model_barang.php
+++ application/models/Model_barang.php
@@ -42,7 +42,15 @@
     }
 
 
-  
+    public function detail_brg($id_brg)
+    {
+      $result = $this->db->where('id_brg', $id_brg)->get('tb_barang');
+      if($result->num_rows()>0){
+        return $result->result();
+        }else{
+          return false;
+      }
+  }
 }
 
 
